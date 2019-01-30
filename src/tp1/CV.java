da65b000629bcaa47b86bbe5aaa44b2317d0c856
Choujaa Yassine <yassine21212@live.fr>
2019-01-29 19:09:05 -0500

    creation de l'objet

--- src/tp1/CV.java
+++ src/tp1/CV.java
@@ -1,5 +1,31 @@
 package tp1;
 
+import java.util.Arrays;
+
 public class CV {
 
+	String nom;
+	 String prenom;
+	 String formation;
+	 int annee;
+	 String[] competence;
+	 String attente;
+		
+	
+
+	public CV(String nom2, String prenom2, String formation2, int annee2, String[] competence2, String attente2) {
+		// TODO Auto-generated constructor stub
+		nom = nom2;
+		prenom = prenom2;
+		formation = formation2;
+		annee = annee2;
+		competence = competence2;
+		attente = attente2;
+	}
+
+	 static String[] compet = {"JAVA","C#","SQL"} ;
+	
+	 static CV pascal = new CV("Raymond", "Pascal", "Etudiant" , (int) 0.5, compet , "Un prof qui explique clairement le cours/but de celui-ci" );
+	 static CV yassine = new CV("Choujaaaa", "Yassine", "Etudiant" , (int) 0,compet, "Aucune" );
+	
 }
